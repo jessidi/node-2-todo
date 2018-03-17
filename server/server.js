a466bd223225809ab8f38340f0c676eb924bc83c
DJ <djxhwill@gmail.com>
2018-03-17 14:53:44 -0700

    Set up seperate test and development envs

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require('./config/config');
+
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -9,7 +11,7 @@ var {User} = require('./models/user');
 
 //local
 var app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 
 app.use(bodyParser.json());
@@ -92,6 +94,9 @@ app.patch('/todos/:id', (req, res) => {
     res.status(400).send();
   })
 });
+
+
+
 
 
 app.listen(port, () => {
