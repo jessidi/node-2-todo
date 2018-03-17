a02262950350a5da80552e8636f7c4fab3bd9f67
DJ <djxhwill@gmail.com>
2018-03-17 09:38:43 -0700

    Test the DELETE /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -105,3 +105,45 @@ it('should return 404 if todo not found', (done) => {
       .end(done);
   });
 });
+
+describe('DELETE /todos/:id', () => {
+  it('should remove a todo',(done) => {
+    var hexId = todos[1]._id.toHexString();
+
+    request(app)
+    .delete(`/todos/${hexId}`)
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.todo._id).toBe(hexId);
+    }).end((err, res) => {
+      if (err){
+        return done(err);
+      }
+
+      //query database using findById
+      Todo.findById(hexId).then((todo) => {
+        expect(todo).toNotExist();
+        done();
+      }).catch((e) => done(e));
+    });
+  });
+
+  it('should return 404 if todo not found', (done) => {
+      //make sure get a 404 back
+      var hexId = new ObjectID().toHexString();
+
+      request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(404)
+      .end(done);
+    });
+
+    it('should return 400 if object id is invalid', (done) => {
+        //todos/123
+        request(app)
+        .delete('/todos/123abc')
+        .expect(400)
+        .end(done);
+    });
+
+});
