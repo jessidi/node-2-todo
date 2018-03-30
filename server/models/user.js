8c4ce1b5ec72a3273547a698c44e0b78582632d4
DJ <djxhwill@gmail.com>
2018-03-29 17:25:47 -0700

    Add tests for POST /users/login

--- server/models/user.js
+++ server/models/user.js
@@ -70,18 +70,20 @@ UserSchema.statics.findByToken = function (token) {
   });
 };
 
-UserSchema.statics.findByCredentials = function(email, password){
+UserSchema.statics.findByCredentials = function (email, password) {
   var User = this;
+
   return User.findOne({email}).then((user) => {
-    if( !user ) {
+    if (!user) {
       return Promise.reject();
     }
+
     return new Promise((resolve, reject) => {
-      //User bcrypt.compare
+      // Use bcrypt.compare to compare password and user.password
       bcrypt.compare(password, user.password, (err, res) => {
-        if(res){
+        if (res) {
           resolve(user);
-        }else {
+        } else {
           reject();
         }
       });
